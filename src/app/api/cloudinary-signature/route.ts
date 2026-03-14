62227745f63aca3ff2c2a6d2086e58977b98d0f3
Pritam <pritambose040@gmail.com>
2026-03-15 00:14:49 +0530

    feat: integrate Cloudinary for secure file uploads by adding configuration and a signature generation API endpoint.

---
+++ src/app/api/cloudinary-signature/route.ts
@@ -0,0 +1,46 @@
+import { handleRequest } from "@/helpers/handleRequest";
+import cloudinary from "@/lib/cloudinary";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { ApiError } from "@/helpers/ApiError";
+import { ApiResponse } from "@/helpers/ApiResponse";
+
+export async function POST() {
+    return handleRequest(async () => {
+        const session = await getServerSession(authOptions);
+        if (!session) {
+            throw new ApiError(401, "You are not authenticated");
+        }
+
+        if (session.user.role !== "creator") {
+            throw new ApiError(403, "Only creators can upload files");
+        }
+
+        const timestamp = Math.floor(Date.now() / 1000);
+
+        const params = {
+            timestamp,
+            folder: `droply/products/${session.user.id}`,
+            max_file_size: 52428800, // 50MB in bytes
+        };
+
+        const signature = cloudinary.utils.api_sign_request(
+            params,
+            process.env.CLOUDINARY_API_SECRET!
+        );
+
+        return NextResponse.json(
+            new ApiResponse(
+                200,
+                "Signature generated successfully",
+                {
+                    ...params,
+                    signature,
+                    apiKey: process.env.CLOUDINARY_API_KEY,
+                    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+                }
+            )
+        );
+    });
+}
